f869d824703547aacde7e4d6c76de790205b8ff2
Redwan-Alif <alifredwan@gmail.com>
2022-06-12 21:08:08 +0600

    fibonacci feat

---
+++ script.js
@@ -0,0 +1,12 @@
+function fibo(c){
+    let num1=0;
+    let num2=1;
+    let num3=0;
+    for ( let n=0; n<c; n++){
+        num3 = num1 + num2;
+        console.log(num3);
+        num1= num2;
+        num2= num3;
+        }
+    }
+    fibo(9);
